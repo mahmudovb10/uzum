ad831369da0c317ecd07b1876746f8d60f91855d
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-08-06 16:20:16 +0500

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,25 +3,25 @@ import { useState, useEffect } from "react";
 
 export const usefetch = (url) => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false); // <== nomi o‘zgartirildi
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
-      setLoading(true); // <== Ispending -> setLoading
+      setLoading(true);
       try {
         const req = await axios(url);
         console.log(req);
-        setData(req.data); // <== faqat req.data olish kifoya
+        setData(req.data);
       } catch (error) {
         console.log(error.message);
-        setError(error.message); // <== xatolik holatini saqlash
+        setError(error.message);
       } finally {
-        setLoading(false); // <== Ispending(false) -> setLoading(false)
+        setLoading(false);
       }
     };
     getData();
   }, [url]);
 
-  return { data, loading, error }; // <== loading nomi to‘g‘rilandi
+  return { data, loading, error };
 };
